65f86ba471a60ac4f78396479c345dca2f85ceae
Kristin Smith <kms@Kristins-MacBook-Pro.local>
2015-01-07 17:31:07 -0500

    Migration creating events table, events model created, users model modified to have many events

---
+++ db/migrate/20150107222703_create_events.rb
@@ -0,0 +1,16 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.timestamps null: false
+      t.string :address
+      t.datetime :ended_at
+      t.float :lat, null: false
+      t.float :lon, null: false
+      t.string :name, null: false
+      t.datetime :started_at, null: false
+      t.integer :user_id, null: false
+    end
+
+    add_index :events, :user_id
+  end
+end
