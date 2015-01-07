e514f84ad9d5ac3536fcfbb1299473b2bcc0646c
Kristin Smith <kms@Kristins-MacBook-Pro.local>
2015-01-07 18:50:40 -0500

    attempted to remove offending syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'webmock/rspec'
 
-# http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+# http://rubydoc.info/gems/nspec-core/rSpec/Core/Configuration
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.syntax = :expect
