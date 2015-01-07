89789900757fb0c57659c36dc8708405a3e46a7c
Kristin Smith <kms@Kristins-MacBook-Pro.local>
2015-01-07 17:32:47 -0500

    factories for events and users added

---
+++ spec/factories.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+
+  factory :event do
+    lat
+    lon
+    name
+    started_at
+    owner factory: :user
+  end
+
+  factory :user do
+    device_token { generate(:token) }
+  end
+end
