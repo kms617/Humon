3f50d25a24ab471e4b0b4c80aca4a5e459ac2129
Kristin Smith <kms@Kristins-MacBook-Pro.local>
2015-01-08 16:30:51 -0500

    associations moved ahead of validations in user and event models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ActiveRecord::Base
+  belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
+
   validates_presence_of :lat
   validates_presence_of :lon
   validates_presence_of :name
   validates_presence_of :started_at
-
-  belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
 end
