7a611b6ecf617eb681069aaebe670989cabc56f6
Kristin Smith <kms@Kristins-MacBook-Pro.local>
2015-01-08 15:22:06 -0500

    additional fields added to events_spec per heroku api testing guidelines, json timestamp conversions added to tests

--- app/views/api/v1/events/_event.json.jbuilder
+++ app/views/api/v1/events/_event.json.jbuilder
@@ -1,12 +1,16 @@
 json.cache! event do
   json.address event.address
+  json.created_at event.created_at
   json.ended_at event.ended_at
   json.id event.id
   json.lat event.lat
   json.lon event.lon
   json.name event.name
   json.started_at event.started_at
+  json.updated_at event.updated_at
   json.owner do
+    json.created_at event.owner.created_at
     json.device_token event.owner.device_token
+    json.updated_at event.owner.updated_at
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,15 +1,14 @@
 require 'faker'
 FactoryGirl.define do
-
   sequence :token do
     SecureRandom.hex(3)
   end
 
   factory :event do
-    lat {Faker::Address.latitude.to_f.round(5)}
-    lon {Faker::Address.longitude.to_f.round(5)}
-    sequence(:name) { |n| "Name #{n}"}
-    sequence(:started_at) { |n| n.hours.from_now}
+    lat { Faker::Address.latitude.to_f.round(5) }
+    lon { Faker::Address.longitude.to_f.round(5) }
+    sequence(:name) { |n| "Name #{n}" }
+    sequence(:started_at) { |n| n.hours.from_now }
     owner factory: :user
   end
 
