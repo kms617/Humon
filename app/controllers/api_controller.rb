1f48de0a417f24f5dccd69b62ca23a4d643fe57f
Kristin Smith <kms@Kristins-MacBook-Pro.local>
2015-01-14 14:59:05 -0500

    Errors for creating a blank event and patching a blank event refactored
    test syntax refactored for multiple lines

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,15 @@
 class ApiController < ApplicationController
   protect_from_forgery with: :null_session
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    render json: { errors: exception.message }, status: :bad_request
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: { errors: exception.message }, status: :not_found
+  end
+
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: { errors: exception.message }, status: :bad_request
+  end
 end
