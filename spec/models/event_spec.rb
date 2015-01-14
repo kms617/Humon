b0be0c3612557a5937066aae01f878692dd9eda2
Kristin Smith <kms@Kristins-MacBook-Pro.local>
2015-01-14 12:15:45 -0500

    address validation added to model spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,7 +6,7 @@ describe Event do
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:lat) }
+    it { should validate_presence_of(:address) }
     it { should validate_presence_of(:lat) }
     it { should validate_presence_of(:lon) }
     it { should validate_presence_of(:name) }
