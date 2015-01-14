bf0663bf0a07c80f19141238019c07acf89039bf
Kristin Smith <kms@Kristins-MacBook-Pro.local>
2015-01-14 10:28:13 -0500

    Json schema used for nearests
    address validation added to model spec

--- spec/requests/api/v1/events/nearest_spec.rb
+++ spec/requests/api/v1/events/nearest_spec.rb
@@ -9,40 +9,7 @@ describe 'GET /v1/events/nearests?lat=&lon=&radius=' do
 
     get v1_events_nearests_path(lat: lat, lon: lon, radius: radius)
 
-    expect(response_json).to eq([
-      {
-        'address' => near_event.address,
-        'created_at' => near_event.created_at.as_json,
-        'ended_at' => near_event.ended_at,
-        'id' => near_event.id,
-        'lat' => near_event.lat,
-        'lon' => near_event.lon,
-        'name' => near_event.name,
-        'started_at' => near_event.started_at.as_json,
-        'updated_at' => near_event.updated_at.as_json,
-        'owner' => {
-          'created_at' => near_event.created_at.as_json,
-          'device_token' => near_event.owner.device_token,
-          'updated_at' => near_event.updated_at.as_json
-        }
-      },
-      {
-        'address' => farther_event.address,
-        'created_at' => farther_event.created_at.as_json,
-        'ended_at' => farther_event.ended_at.as_json,
-        'id' => farther_event.id,
-        'lat' => farther_event.lat,
-        'lon' => farther_event.lon,
-        'name' => farther_event.name,
-        'started_at' => farther_event.started_at.as_json,
-        'updated_at' => farther_event.updated_at.as_json,
-        'owner' => {
-          'created_at' => farther_event.created_at.as_json,
-          'device_token' => farther_event.owner.device_token,
-          'updated_at' => farther_event.updated_at.as_json
-        }
-      }
-    ])
+    expect(response).to match_response_schema(:nearest)
   end
 
   it 'returns an error message when no event is found' do
